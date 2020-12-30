7a4b36cd7e650a71cda036b7366c5749d1e3b26d
fi6 <flappyice73@outlook.com>
2020-12-30 18:44:16 +0000

    updated alert function

--- src/commands/arena.js
+++ src/commands/arena.js
@@ -133,7 +133,7 @@ async function findArenaMsg() {
 
 async function findArena(args, msg) {
     const content = await findArenaMsg(args, msg);
-    // console.log('content', content)
+    console.log('content', content)
     if (!content) { return sendMsg('find', 'fail', [msg]) }
     // console.log('run to here! find arena')
     return sendMsg('find', 'success', [msg, null, content])
@@ -163,31 +163,32 @@ async function alertArena(args, msg) {
         timeLimit = 12 * 60 * 6e4;
     }
     // check args
-    if (!args.length) {
-        return sendMsg('alert', 'help');
+    if (args.length != 1) {
+        return sendMsg('alert', 'help', [msg]);
     }
-    // find profile
-    let profile = Profile.findById(msg.authorId).exec();
-    if (!profile.length) {
-        return sendMsg('alert', 'no_account', [msg])
-    }
-    // time limit
-    if (Date.now() - profile.alertUsedAt < timeLimit) {
-        return sendMsg('alert', 'time_limit', [msg])
-    }
+    // --------find profile--------
+    // let profile = Profile.findById(msg.authorId).exec();
+    // if (!profile.length) {
+    //     return sendMsg('alert', 'no_account', [msg])
+    // }
+    // // time limit
+    // if (Date.now() - profile.alertUsedAt < timeLimit) {
+    //     return sendMsg('alert', 'time_limit', [msg])
+    // }
     // find arena for alert
-    const arena = Arena.findById(msg.authorId).exec();
+    const arena = await Arena.findById(msg.authorId).exec();
     if (!arena) {
         return sendMsg('alert', 'no_arena', [msg])
     }
-    // alert
-    Profile.findByIdAndUpdate(msg.authorId, { alertUsedAt: Date.now() }, (err, res) => {
-        if (err) {
-            console.error(err);
-            return sendMsg('alert', 'error')
-        }
-        return sendMsg('alert', 'success', [msg, arena, args]);
-    })
+    // --------alert--------
+    // Profile.findByIdAndUpdate(msg.authorId, { alertUsedAt: Date.now() }, (err, res) => {
+    //     if (err) {
+    //         console.error(err);
+    //         return sendMsg('alert', 'error')
+    //     }
+    //     return sendMsg('alert', 'success', [msg, arena, args]);
+    // })
+    return sendMsg('alert', 'success', [msg, arena, args[0]]);
 }
 
 
@@ -209,6 +210,9 @@ async function sendMsg(command, type, vars = []) {
     const arenaMsg = await arenaMsgBuilder(command, type, vars);
     // console.log(arenaMsg);
     if (arenaMsg) {
+        if (vars[0].channelId === undefined) {
+            console.error('PLEASE CHECK VARS INPUT');
+        }
         bot.sendChannelMessage(9, vars[0].channelId, arenaMsg, vars[0].msgId);
     }
 }
@@ -218,7 +222,7 @@ async function arenaMsgBuilder(command, type, vars = []) {
     let msg, arena;
     try {
         msg = vars[0];
-        arena = vars[1]
+        arena = vars[1];
     } catch (error) {
         console.error(error)
     }
@@ -230,15 +234,15 @@ async function arenaMsgBuilder(command, type, vars = []) {
                 case 'fail':
                     return '创建失败，请检查房间号、密码格式，并确认房间信息文字长度小于8。'
                 case 'help':
-                    return '创建房间请发送：`.建房 房间号 密码 房间信息 (留言)`\n如：`.建房 BPTC1 147 港服3人 萌新马里奥想找个剑人练习~`\n房间号、密码、房间信息必填，留言选填。';
+                    return '创建房间请发送：`.建房 房间号 密码 房间信息 (留言)`\n如：`.建房 BPTC1 147 港服3人 娱乐房，随便玩~`\n房间号、密码、房间信息必填，留言选填。';
                 default:
                     break;
             }
         },
-        'find': (type) => {
+        'find': async (type) => {
             switch (type) {
                 case 'fail':
-                    return '当前没有房间。\n---\n' + arenaMsgBuilder('create', 'help')
+                    return '当前没有房间。\n---\n' + await arenaMsgBuilder('create', 'help')
                 case 'success':
                     return vars[2];
                 default:
@@ -256,7 +260,14 @@ async function arenaMsgBuilder(command, type, vars = []) {
         'alert': (type) => {
             switch (type) {
                 case 'success':
-                    break;
+                    // console.log(arena, 'arena');
+                    const msgBlock = ''.concat('```markdown\n',
+                        `房间：[${arena.arenaId} ${arena.password}] (${arena.arenaInfo})\n`,
+                        `留言：${arena.remark} (创建于${format_time(arena.createdAt)})`, '```');
+                    return ''.concat(
+                        '(met)all(met) ' + `${vars[2]} ` + `(${msg.author.nickname}的房间广播)\n`,
+                        msgBlock
+                    )
                 case 'no_account':
                     return
                 case 'time_limit':
@@ -265,6 +276,7 @@ async function arenaMsgBuilder(command, type, vars = []) {
                     return '出bug了'
                 case 'help':
                 default:
+                    return '请发送`.房间 广播 广播语`，房间将被广播给所有人。\n如：`.房间 广播 求个剑人，想练对策`'
                     break;
             }
 
@@ -274,7 +286,7 @@ async function arenaMsgBuilder(command, type, vars = []) {
                 '```\n【创建房间】将房间添加至房间列表（覆盖之前的）\n[.建房/.开房 房间号 密码 房间信息 留言]\n[.房间 创建 房间号 密码 房间信息 留言]```\n',
                 '```\n【查看房间】查看房间列表中的房间\n[.找房]\n[.房间 查看]```\n',
                 '```\n【关闭房间】删除房间列表中自己的房间\n[.关房]\n[.房间 关闭/删除]```\n',
-                '```\n【广播找人】将自己创建的房间发送广播提醒\n[.房间 广播]```\n',
+                '```\n【广播找人】将自己创建的房间发送广播提醒\n[.房间 广播 广播语]```\n',
                 '```\n【特训】：教练组专属，可自动发送提醒，其他人需排队加入房间。\n[.房间 特训 房间号 密码 加速 人数限制 留言]\n[.房间 排队 @房主]```'
             );
             return content
