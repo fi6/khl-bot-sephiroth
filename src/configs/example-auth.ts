b1b9182bfd2187d39b1a42103958b2216deefab0
FlappyIce <flappyice73@outlook.com>
2021-01-03 10:48:06 +0000

    update .gitignore

---
+++ src/configs/example-auth.ts
@@ -0,0 +1,8 @@
+export default {
+    mongourl: 'your mongourl',
+    mongouser: 'your mongo user',
+    mongopass: 'your mongo password',
+    khlkey: 'your kaiheila encrypt key',
+    khltoken: 'your kaiheila token',
+    khlverify: 'your kaiheila verify token',
+};
