c28058ee4a630a0435f7f34340dbd1ecc03ec68f
FlappyIce <flappyice73@outlook.com>
2021-07-17 01:06:45 -0400

    updated to 30min

--- src/commands/arena/socket/arena.create.socket.ts
+++ src/commands/arena/socket/arena.create.socket.ts
@@ -18,7 +18,7 @@ class ArenaCreateSocket implements SocketCommandInterface {
         }
         const arena = await this.create(id, args[0], args[1], args[2]);
         fn(
-            `创建成功，有效期1小时。其他人可以通过' .找房 '搜索到此房间。\n${arena.toInfoString()}\n语音、排队等功能请查看说明：https://b23.tv/MaohFT`
+            `创建成功，有效期30分钟。其他人可以通过' .找房 '搜索到此房间。\n${arena.toInfoString()}\n语音、排队等功能请查看说明：https://b23.tv/MaohFT`
         );
     };
     argsChecker(args?: string[]) {
@@ -48,7 +48,7 @@ class ArenaCreateSocket implements SocketCommandInterface {
         title: string
     ) => {
         const expire = new Date();
-        expire.setMinutes(expire.getMinutes() + 60);
+        expire.setMinutes(expire.getMinutes() + 30);
         const arena = await AnonArena.findByIdAndUpdate(
             id,
             {
