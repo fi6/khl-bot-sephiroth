b94af03457f00c919383afda52e92fc5bc3ba7d1
FlappyIce <flappyice73@outlook.com>
2021-07-17 17:19:17 -0400

    updated text

--- src/commands/arena/socket/arena.list.socket.ts
+++ src/commands/arena/socket/arena.list.socket.ts
@@ -11,7 +11,7 @@ class ArenaListSocket implements SocketCommandInterface {
     ) => {
         const arenas = await arenaGetValid();
         if (!arenas.length) {
-            fn('当前没有房间，你可以前往创建。\n发送`.帮助`以获取帮助。');
+            fn('当前没有房间，你可以进行创建。\n发送 .帮助 以获取帮助。');
             return;
         } else {
             let text = '房间列表';
@@ -19,7 +19,7 @@ class ArenaListSocket implements SocketCommandInterface {
                 text += '\n';
                 text += arena.toInfoString();
             });
-            text += '\n非公开房间需要加入。发送`.帮助`以获取帮助。';
+            text += '\n星号密码需加入才能查看。\n发送 .帮助 以获取帮助。';
             fn(text);
         }
     };
