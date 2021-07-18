0ebc04254a103d464927a530e0a5da44e3ea46d6
FlappyIce <flappyice73@outlook.com>
2021-07-18 01:23:09 -0400

    updated text

--- src/commands/arena/socket/arena.list.socket.ts
+++ src/commands/arena/socket/arena.list.socket.ts
@@ -14,12 +14,24 @@ class ArenaListSocket implements SocketCommandInterface {
             fn('当前没有房间，你可以进行创建。\n发送 .帮助 以获取帮助。');
             return;
         } else {
-            let text = '房间列表';
+            const headers = [
+                '房间列表',
+                '全部房间',
+                '查询房间',
+                '找房功能',
+                '房间查询',
+            ];
+            let text = headers[Math.floor(Math.random() * headers.length)];
             arenas.forEach((arena) => {
                 text += '\n';
                 text += arena.toInfoString();
             });
-            text += '\n非公开房间需加入查看密码。\n发送 .帮助 以获取帮助。';
+            const tails = [
+                '如需使用帮助，请发送 .帮助',
+                '发送 .帮助以获取帮助',
+                '非公开房间需加入，请发送 .帮助',
+            ];
+            text += '\n' + tails[Math.floor(Math.random() * tails.length)];
             fn(text);
         }
     };
